ee09c832c1a0fd03682668612786876b43c2751b
Gytis <gytislapenas@gmail.com>
2018-04-05 18:48:41 +0300

    Uzduotys

--- 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
+++ 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
@@ -1,19 +1,36 @@
-/*SEARCH-BOX TOGGLE*/
+/*LOGINCONTAINER BAR START*/
 $(document).ready(function(){
-	$(".active").click(function(){
-		$(".search-box").toggle();
-		event.preventDefault(); /*Prevents default behavior. Prevents from jumping to the top. */
+	$('.logInbar').on('click', function(){
+		//tikrina ar meniu yra ijungtas
+		$('.loginContainerOverlay').on('click', function(){
+			if($('.loginContainerOverlay').hasClass('open')){
+				$('.loginContainerOverlay').removeClass('open');
+				$('#loginContainer').css('display','none');
+			}
+		});
+		//jeigu nebuvo ijungtas ijungia
+		$('.loginContainerOverlay').addClass('open');/*Use the jQuery addClass() function to give the classes animated and shake to all your elements with the class well.
+*/
+		$('#loginContainer').slideToggle(function(){
+		});
 	});
 });
-/*SEARCH-BOX END*/
-
-/*LOGINCONTAINER BAR PART*/
-$(document).ready(function(){
-	$(".logInbar").click(function(){
-		$("#loginContainer").toggle();
-		$("input[type='text']").focus();
-		event.preventDefault();
-	});
-});
-
 /*LOGINCONTAINER BAR END*/
+
+
+/*SEARCHBOX BAR START*/
+	//tikrina ar meniu yra ijungtas
+	$('.active').on('click', function(){
+		$('.searchBoxOverlay').on('click', function(){
+			if($('.searchBoxOverlay').hasClass('openSearchBox')){
+				$('.searchBoxOverlay').removeClass('openSearchBox');
+				$('.search-box').css('display', 'none');
+			}
+		});
+		//jeigu nebuvo ijungtas ijungia
+		$('.searchBoxOverlay').addClass('openSearchBox');/*Use the jQuery addClass() function to give the classes animated and shake to all your elements with the class well.
+*/
+		$('.search-box').slideToggle(function(){
+		});
+	});
+/*SEARCHBOX BAR END*/
