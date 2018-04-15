366d497942265e57a63f1b3e90edd8186f424372
Gytis <gytislapenas@gmail.com>
2018-04-15 16:58:54 +0300

    Uzduotys

--- 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
+++ 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
@@ -37,6 +37,69 @@ $(document).ready(function(){
 
 
 
+
+
+/*LOGIN WITH SOCIAL MEDIA SITES (FACEBOOK START)*/
+
+/*This is reable name. Instead of 'response' you can write anything you whant*/
+function logInFb() {
+	FB.Login(function (response) {
+		console.log(response);
+	}, {scope: 'email, public_profile'})permissions
+}
+
+/*i fb neisikelia aplikacija*/ /*pasileisti su xampp tinklalapi*/
+
+
+/*window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '1395791073900830',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.12'
+    });/*
+    	FB.getLoginStatus(function (response){
+    		if (response.status==='connect') {
+    			document.getElementById('fb').innerHTML='Jūs prisijungėte';
+    		}else if(response.status='not_authorized'){
+    			document.getElementById('fb').innerHTML='Klaida. Patikrinkite savo prisijungimo duomenis';
+    		}else{
+    			document.getElementById('fb').innerHTML='Jūs neprisijungėte. Patikrinkite savo prisijungimo duomenis ir bandykite dar kartą';
+    		}
+    	});
+
+    	FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+  function logInFb(){
+  	FB.login(function(response){
+  		if (response.status==='connect') {
+    			document.getElementById('fb').innerHTML='Jūs prisijungėte';
+    		}else if(response.status='not_authorized'){
+    			document.getElementById('fb').innerHTML='Klaida. Patikrinkite savo prisijungimo duomenis';
+    		}else{
+    			document.getElementById('fb').innerHTML='Jūs neprisijungėte. Patikrinkite savo prisijungimo duomenis ir bandykite dar kartą';
+    		}
+  	});
+  }*/
+
+/*LOGIN WITH SOCIAL MEDIA SITES (FACEBOOK END)*/
+
+
+
+
+
+
+
+
 /*SEARCHBOX BAR START*/
 	//tikrina ar meniu yra ijungtas
 	$('.active').on('click', function(){
