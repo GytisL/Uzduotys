6e0447d4f7fb119477acf10fd58b0a857caa1dbe
Gytis <gytislapenas@gmail.com>
2018-04-19 22:22:31 +0300

    Uzduotys

--- 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
+++ 5 uzduotis naujienu tinklalapis/NaujienuTinklalapis.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 /*LOGINCONTAINER BAR END*/
 
 
-/*ROTATE FROM loginContainer TO signUpContainer (START)*/
+/*ROTATE FROM loginContainer TO signUpContainer (START)
 $(document).ready(function(){
 	$('.to_register').on('click', function(){
 		$('.to_signup').addClass('switch');
@@ -42,23 +42,67 @@ $(document).ready(function(){
 /*LOGIN WITH SOCIAL MEDIA SITES (FACEBOOK START)*/
 
 
-
-
-
+/* 1#
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '{227999751083929}',
       cookie     : true,
       xfbml      : true,
-      version    : 'v2.8'
+      version    : 'v2.12'
+    });
+
+    FB.getLoginStatus(function(response){
+      if (response.status === 'connected') {
+        document.getElementById('status').innerHTML = 'Jus esate neprisijunges';
+      }else if (response.status === 'not authorized') {
+          document.getElementById('status').innerHTML = 'Prisijungete'
+      }else {
+          document.getElementById('status').innerHTML = 'Jus neprisijungete prie facebook'
+      }
+    });
+  };
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+function login() {
+  FB.login(function(response) {
+    if (response.status === 'connected') {
+        document.getElementById('status').innerHTML = 'Jus esate neprisijunges';
+    }else if (response.status === 'not authorized') {
+          document.getElementById('status').innerHTML = 'Prisijungete'
+    }else {
+          document.getElementById('status').innerHTML = 'Jus neprisijungete prie facebook'
+    }
+  });
+}
+
+1*/
+
+
+  /*2
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{227999751083929}',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.12'
     });
       
     
+
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
-    });   
-      
+    });  
+
   };
+
+
 
   (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -70,25 +114,14 @@ $(document).ready(function(){
 
 function statusChangeCallback(response){
   if(response.status === 'connected'){
-    console.log('Logged in');
+    console.log('Logged in and authenticated');
   }else{
     console.log('Not authenticated');
   }
 }
 
 
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-
-
-
-
-
+2*/
 
 
 
@@ -96,19 +129,24 @@ function checkLoginState() {
 /*function logInFb() {
 	FB.Login(function (response) {
 		console.log(response);
-	}, {scope: 'email, public_profile'})permissions
+	}, {scope: 'email, public_profile'})
 }*/
 
-/*i fb neisikelia aplikacija*/ /*pasileisti su xampp tinklalapi*/
 
 
-/*window.fbAsyncInit = function() {
+
+/*paleidus su xampp meta error: Insecure Login Blocked: You can't get an access token or log in to this app from an insecure page.
+ Try re-loading the page as https://*/
+ /*paleidus is papkes ta pati faila meta error:Nepavyksta atverti nuorodos: Nuorodos domenas nėra įtrauktas į programėlės domenų sąrašą. 
+ Programėlės nustatymuose, lauke Programėlės Domenai, pridėkite visus programėlės domenus ir subdomenus, ir jūs sėkmingai galėsite atidaryti šią nuorodą.*/
+
+window.fbAsyncInit = function() {
     FB.init({
-      appId            : '1395791073900830',
+      appId            : '227999751083929',
       autoLogAppEvents : true,
       xfbml            : true,
       version          : 'v2.12'
-    });/*
+    });
     	FB.getLoginStatus(function (response){
     		if (response.status==='connect') {
     			document.getElementById('fb').innerHTML='Jūs prisijungėte';
@@ -130,7 +168,8 @@ function checkLoginState() {
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-  function logInFb(){
+
+  function login(){
   	FB.login(function(response){
   		if (response.status==='connect') {
     			document.getElementById('fb').innerHTML='Jūs prisijungėte';
@@ -139,8 +178,8 @@ function checkLoginState() {
     		}else{
     			document.getElementById('fb').innerHTML='Jūs neprisijungėte. Patikrinkite savo prisijungimo duomenis ir bandykite dar kartą';
     		}
-  	});
-  }*/
+  	}), {scope: 'email, public_profile'};
+  }
 
 /*LOGIN WITH SOCIAL MEDIA SITES (FACEBOOK END)*/
 
